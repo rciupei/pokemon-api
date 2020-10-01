27ecb02cfac7766086142fa115aaa39c07d3e491
Razvan <rciupei@yahoo.com>
2020-10-01 20:30:01 +0300

    improved display fixed console errors/warnings

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,35 +7,36 @@ import './Pokemon.css'
 
 export default class Pokemon extends Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
-		this.state = {imageUrl: null, data:{}}
+		this.state = { imageUrl: null, data: {} }
 		this.renderTypes = this.renderTypes.bind(this)
 	}
 
-	async componentDidMount(){
+	async componentDidMount() {
 		const res = await axios.get(this.props.url)
-		this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default})
+		this.setState({ data: res.data, imageUrl: res.data.sprites.other.dream_world.front_default })
 	}
 
-	renderTypes(){
-		if(this.state.data.types){
-			return this.state.data.types.map(type=>{
-				return (<PokemonType key={type.type.name} type={type.type.name}/>)
+	renderTypes() {
+		if (this.state.data.types) {
+			return this.state.data.types.map(type => {
+				return (<PokemonType key={type.type.name} type={type.type.name} />)
 			})
 		}
 	}
 
 	render() {
-		if(this.state.imageUrl){
+		if (this.state.imageUrl) {
+			let { name } = this.state.data.species;
 			return (
 				<div className="Pokemon">
 					<Link to={`/pokemon/${this.state.data.species.name}`}>
-						<div class="col mb-4">
+						<div className="col mb-4">
 							<div className="card">
 								<img className="card-img-top" src={this.state.imageUrl} alt={this.props.name} />
 								<div className="card-body mt-2">
-									<h5 className="card-title">{this.state.data.species.name}</h5>
+									<h5 className="card-title">  {name.charAt(0).toUpperCase() + name.slice(1)}</h5>
 									<p className="card-text">{this.renderTypes()}</p>
 								</div>
 							</div>

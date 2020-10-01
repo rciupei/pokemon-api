cc9697dd97336a837b8d2fabe2f2ddc8fd7fa0ae
Razvan <rciupei@yahoo.com>
2020-10-01 20:10:10 +0300

    improved display fixed console errors/warnings

--- src/components/BigPokemon.js
+++ src/components/BigPokemon.js
@@ -1,115 +1,180 @@
-import React, { Component } from 'react'
-import pokeApi from '../api/pokeApi'
+import React, { Component } from "react";
+import pokeApi from "../api/pokeApi";
 
-import PokemonType from './PokemonType'
-import Spinner from './Spinner'
-import './BigPokemon.css'
+import PokemonType from "./PokemonType";
+import Spinner from "./Spinner";
+import "./BigPokemon.css";
 
 export default class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageUrl: null, data: {}, isLoading: true, error: false };
+    this.renderTypes = this.renderTypes.bind(this);
+    this.renderAbilities = this.renderAbilities.bind(this);
+    this.renderMoves = this.renderMoves.bind(this);
+  }
 
-	constructor(props){
-		super(props)
-		this.state = {imageUrl: null, data:{}, isLoading: true, error: false}
-		this.renderTypes = this.renderTypes.bind(this)
-		this.renderAbilities = this.renderAbilities.bind(this)
-		this.renderMoves = this.renderMoves.bind(this)
-	}
+  async componentDidMount() {
+    try {
+      const res = await pokeApi.get(
+        `/pokemon/${this.props.match.params.searchTerm}`
+      );
+      this.setState({
+        data: res.data,
+        imageUrl: res.data.sprites.other.dream_world.front_default,
+        isLoading: false,
+        error: false,
+      });
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+    }
+  }
 
-	async componentDidMount(){
-		try {
-			const res = await pokeApi.get(`/pokemon/${this.props.match.params.searchTerm}`)
-			this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default, isLoading: false, error: false})	
-		} catch (error) {
-			this.setState({error: true, isLoading: false})
-		}
-	}
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.match.params.searchTerm !== this.props.match.params.searchTerm
+    ) {
+      try {
+        this.setState({ isLoading: true });
+        const res = await pokeApi.get(
+          `/pokemon/${this.props.match.params.searchTerm}`
+        );
+        this.setState({
+          data: res.data,
+          imageUrl: res.data.sprites.other.dream_world.front_default,
+          isLoading: false,
+          error: false,
+        });
+      } catch (error) {
+        this.setState({ error: true, isLoading: false });
+      }
+    }
+  }
 
-	async componentDidUpdate(prevProps, prevState){
-		if (prevProps.match.params.searchTerm !== this.props.match.params.searchTerm){
-			try {
-				this.setState({isLoading: true})
-				const res = await pokeApi.get(`/pokemon/${this.props.match.params.searchTerm}`)
-				this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default, isLoading: false, error: false})	
-			} catch (error) {
-				this.setState({error: true, isLoading: false})
-			}
-		}
-	}
+  renderTypes() {
+    if (this.state.data.types) {
+      return this.state.data.types.map((type) => {
+        return <PokemonType key={type.type.name} type={type.type.name} />;
+      });
+    }
+  }
 
-	renderTypes(){
-		if(this.state.data.types){
-			return this.state.data.types.map(type=>{
-				return (<PokemonType key={type.type.name} type={type.type.name}/>)
-			})
-		}
-	}
+  renderAbilities() {
+    return this.state.data.abilities.map((ability) => {
+      return (
+        <li className="list-group-item" key={ability.ability.name}>
+          {ability.ability.name}
+        </li>
+      );
+    });
+  }
 
-	renderAbilities(){
-		return this.state.data.abilities.map(ability=>{
-			return <li className="list-group-item">{ability.ability.name}</li>
-		})
-	}
+  renderMoves() {
+    return this.state.data.moves.map((move) => {
+      return <li className="list-group-item">{move.move.name}</li>;
+    });
+  }
 
-	renderMoves(){
-		return this.state.data.moves.map(move=>{
-			return <li className="list-group-item">{move.move.name}</li>
-		})
-	}
+  render() {
+    if (this.state.isLoading) {
+      return <Spinner />;
+    }
+    if (this.state.error) {
+      return <h3>Error: Unable to find pokemon</h3>;
+    }
+    let { name } = this.state.data.species;
 
-	render() {
-		if(this.state.isLoading){
-			return <Spinner />
-		}
-		if(this.state.error){
-			return(
-				<h3>Error: Unable to find pokemon</h3>
-			)
-		}
-			return(
-				<div className="container">
-					<div className="BigPokemon card mb-3">
-						<div className="row no-gutters">
-								<div className="col-md-4">
-									<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-										<ol className="carousel-indicators">
-											<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-											<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-											<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-										</ol>
-										<div className="carousel-inner">
-											<div className="carousel-item active">
-												<img src={this.state.data.sprites.other.dream_world.front_default} className="d-block w-100" alt="..." />
-											</div>
-											<div className="carousel-item">
-												<img src={this.state.data.sprites.front_default} className="d-block w-100" alt="front_default" />
-											</div>
-											<div className="carousel-item">
-												<img src={this.state.data.sprites.back_default} className="d-block w-100" alt="back_default" />
-											</div>
-										</div>
-										<a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-											<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-											<span className="sr-only">Previous</span>
-										</a>
-										<a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-											<span className="carousel-control-next-icon" aria-hidden="true"></span>
-											<span className="sr-only">Next</span>
-										</a>
-									</div>
-							</div>
-							<div className="col-md-8">
-								<div className="card-body">
-									<h2 className="card-title">{this.state.data.species.name}</h2>
-										<ul className="list-group my-3">
-											<li className="list-group-item active">Abilities</li>
-											{this.renderAbilities()}
-										</ul>
-									<p className="card-text my-3">{this.renderTypes()}</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			)
-		}
+    return (
+      <div className="container h-100 mt-2 d-flex justify-content-center flex-column ">
+        <div className="BigPokemon card mb-3 ">
+          <div className="row no-gutters ">
+            <div className="col-md-4">
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide"
+                data-ride="carousel"
+              >
+                <ol className="carousel-indicators">
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    className="active"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src={
+                        this.state.data.sprites.other.dream_world.front_default
+                      }
+                      className="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={this.state.data.sprites.front_default}
+                      className="d-block w-100"
+                      alt="front_default"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={this.state.data.sprites.back_default}
+                      className="d-block w-100"
+                      alt="back_default"
+                    />
+                  </div>
+                </div>
+                <a
+                  className="carousel-control-prev"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h2 className="card-title">
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </h2>
+                <ul className="list-group my-3">
+                  <li className="list-group-item active">Abilities</li>
+                  {this.renderAbilities()}
+                </ul>
+                <p className="card-text my-3">{this.renderTypes()}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,49 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
-import PokemonType from './PokemonType'
-import './Pokemon.css'
+import PokemonType from "./PokemonType";
+import "./Pokemon.css";
 
 export default class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageUrl: null, data: {} };
+    this.renderTypes = this.renderTypes.bind(this);
+  }
 
-	constructor(props){
-		super(props)
-		this.state = {imageUrl: null, data:{}}
-		this.renderTypes = this.renderTypes.bind(this)
-	}
+  async componentDidMount() {
+    const res = await axios.get(this.props.url);
+    this.setState({
+      data: res.data,
+      imageUrl: res.data.sprites.other.dream_world.front_default,
+    });
+  }
 
-	async componentDidMount(){
-		const res = await axios.get(this.props.url)
-		this.setState({data: res.data,imageUrl: res.data.sprites.other.dream_world.front_default})
-	}
+  renderTypes() {
+    if (this.state.data.types) {
+      return this.state.data.types.map((type) => {
+        return <PokemonType key={type.type.name} type={type.type.name} />;
+      });
+    }
+  }
 
-	renderTypes(){
-		if(this.state.data.types){
-			return this.state.data.types.map(type=>{
-				return (<PokemonType key={type.type.name} type={type.type.name}/>)
-			})
-		}
-	}
-
-	render() {
-		if(this.state.imageUrl){
-			return (
-				<div className="Pokemon">
-					<Link to={`/pokemon/${this.state.data.species.name}`}>
-						<div class="col mb-4">
-							<div className="card">
-								<img className="card-img-top" src={this.state.imageUrl} alt={this.props.name} />
-								<div className="card-body mt-2">
-									<h5 className="card-title">{this.state.data.species.name}</h5>
-									<p className="card-text">{this.renderTypes()}</p>
-								</div>
-							</div>
-						</div>
-					</Link>
-				</div>
-			)
-		}
-		return null
-	}
+  render() {
+    if (this.state.imageUrl) {
+      let { name } = this.state.data.species;
+      return (
+        <div className="Pokemon">
+          <Link to={`/pokemon/${name}`}>
+            <div className="col mb-4">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={this.state.imageUrl}
+                  alt={this.props.name}
+                />
+                <div className="card-body mt-2">
+                  <h5 className="card-title">
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </h5>
+                  <p className="card-text">{this.renderTypes()}</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      );
+    }
+    return null;
+  }
 }
